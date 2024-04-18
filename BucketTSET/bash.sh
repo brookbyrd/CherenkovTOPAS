5dd25beba6af3b3220e5d264684ef65ced414596
brookbyrd <78179495+brookbyrd@users.noreply.github.com>
2024-04-18 02:16:25 -0400

    Add files via upload

---
+++ BucketTSET/bash.sh
@@ -0,0 +1,11 @@
+export TOPAS_G4_DATA_DIR=/Applications/G4Data
+export QT_QPA_PLATFORM_PLUGIN_PATH=/Applications/topas/Frameworks
+
+# Path to your TOPAS binary
+TOPAS="/Applications/topas/bin/topas"
+
+
+$TOPAS APGantryN.topas
+
+
+
